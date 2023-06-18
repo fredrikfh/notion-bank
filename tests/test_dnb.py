e0c466490ac93bc85a2a583ff745b95e791e730e
fredrfha <fredrfha@stud.ntnu.no>
2023-06-18 13:01:55 +0200

    Add tests

---
+++ tests/test_dnb.py
@@ -0,0 +1,24 @@
+from banks.dnb import get_transactions, create_pages
+from notion_client import Client
+from dotenv import load_dotenv
+import os
+
+import pytest
+
+load_dotenv()
+
+notion = Client(auth=os.getenv('NOTION_API_KEY'))
+transactions = get_transactions('tests/fixtures/dnb.txt')
+transactions_expected = [{'Dato': '19.05.2023', 'Forklaring': 'Supermercado Expres ', 'Rentedato': '19.05.2023', 'Ut fra konto': '2500', 'Inn på konto': '', 'belop': -2500.0}, {'Dato': '17.05.2023', 'Forklaring': 'Sopping A ',
+                                                                                                                                                                                 'Rentedato': '19.05.2023', 'Ut fra konto': '', 'Inn på konto': '6000', 'belop': 6000.0}, {'Dato': '15.05.2023', 'Forklaring': 'Payment B', 'Rentedato': '15.05.2023', 'Ut fra konto': '126', 'Inn på konto': '', 'belop': -126.0}]
+pages = create_pages(transactions)
+pages_exprected = [{'Dato': {'date': {'start': '2023-05-19'}}, 'Beskrivelse': {'title': [{'text': {'content': 'Supermercado Expres '}}]}, 'Beløp': {'number': -2500.0}}, {'Dato': {'date': {'start': '2023-05-17'}}, 'Beskrivelse': {
+    'title': [{'text': {'content': 'Sopping A '}}]}, 'Beløp': {'number': 6000.0}}, {'Dato': {'date': {'start': '2023-05-15'}}, 'Beskrivelse': {'title': [{'text': {'content': 'Payment B'}}]}, 'Beløp': {'number': -126.0}}]
+
+
+def test_get_transactions():
+    assert transactions == transactions_expected
+
+
+def test_create_pages():
+    assert pages == pages_exprected
